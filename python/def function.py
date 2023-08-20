7d7e5f98c682b7659f6bad1618cc0f991a2cd00d
Gowthami512 <113289159+Gowthami512@users.noreply.github.com>
2023-08-20 12:45:14 +0530

    Add files via upload

---
+++ python/def function.py
@@ -0,0 +1,15 @@
+def first(a,b):
+    a=a*2
+    b=b*2
+    return a,b
+
+def second(a,b):
+     print("a value is",a)
+     print("b value is",b)
+
+a=int(input())
+b=int(input())
+a,b=first(a,b)
+print(a,b)
+     
+    
