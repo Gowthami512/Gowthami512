c8bfe17cfc9a646bb2c4320d37b29b6b1da51c8d
Gowthami P <113289159+Gowthami512@users.noreply.github.com>
2023-10-11 15:10:30 +0530

    Create timesTwo.js

---
+++ Js/timesTwo.js
@@ -0,0 +1,8 @@
+// Task 1: Code the timesTwo function declaration
+function tiemsTwo(number) {
+    return 2*number;
+    
+}
+
+// Task 2: Export the timesTwo function as a module
+module.exports = tiemsTwo;
