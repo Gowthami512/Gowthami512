51caa28aebf7d887f1356b97c59a6cee383efb42
Gowthami P <113289159+Gowthami512@users.noreply.github.com>
2023-10-16 13:54:53 +0530

    Create 04_Number_Line_Jumb_2.py

---
+++ python/MountBlue_Coding Challenge/04_Number_Line_Jumb_2.py
@@ -0,0 +1,58 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'kangaroo' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER x1
+#  2. INTEGER v1
+#  3. INTEGER x2
+#  4. INTEGER v2
+#
+
+def kangaroo(x1, v1, x2, v2):
+    # Write your code here
+    win=""
+    limit=10000
+    kang1=x1+v1
+    kang2=x2+v2
+    for jumb in range(1,limit+1):
+        if(kang1==kang2):
+            win="YES"
+            break
+        elif(jumb==limit):
+            win="NO"
+            break
+        else:
+            kang1+=v1
+            kang2+=v2
+            
+    return win        
+            
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    x1 = int(first_multiple_input[0])
+
+    v1 = int(first_multiple_input[1])
+
+    x2 = int(first_multiple_input[2])
+
+    v2 = int(first_multiple_input[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
